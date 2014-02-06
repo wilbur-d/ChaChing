f3e9622c72f86f418ff973ec465e4c6c8d902148
wilbur-d <tosh.lyons@gmail.com>
2014-02-05 22:16:08 -0600

    add retry request method to handle cryptsy requests
    and retry if they fail since we get 502's so often

--- ChaChing.py
+++ ChaChing.py
@@ -1,11 +1,29 @@
 import urllib2
 import datetime
+import time
 from threading import Timer
 
 from Cryptsy import Cryptsy
 
 MARKET_IDS = {u'FRK/BTC': 33, u'CGB/BTC': 70, u'FRC/BTC': 39, u'SBC/BTC': 51, u'BTB/BTC': 23, u'NET/XPM': 104, u'ANC/BTC': 66, u'UNO/BTC': 133, u'GLC/BTC': 76, u'ASC/XPM': 112, u'IXC/BTC': 38, u'MEC/BTC': 45, u'DOGE/LTC': 135, u'CLR/BTC': 95, u'WDC/LTC': 21, u'MST/LTC': 62, u'CMC/BTC': 74, u'Points/BTC': 120, u'XNC/LTC': 67, u'CPR/LTC': 91, u'MOON/LTC': 145, u'NET/BTC': 134, u'BTG/BTC': 50, u'CRC/BTC': 58, u'XJO/BTC': 115, u'EMD/BTC': 69, u'42/BTC': 141, u'TAG/BTC': 117, u'NVC/BTC': 13, u'FTC/BTC': 5, u'QRK/BTC': 71, u'DVC/BTC': 40, u'DBL/LTC': 46, u'PHS/BTC': 86, u'PTS/BTC': 119, u'NEC/BTC': 90, u'ZET/BTC': 85, u'CAP/BTC': 53, u'PPC/BTC': 28, u'TIPS/LTC': 147, u'TRC/BTC': 27, u'LTC/BTC': 3, u'SRC/BTC': 88, u'OSC/BTC': 144, u'STR/BTC': 83, u'BUK/BTC': 102, u'CSC/BTC': 68, u'GLD/BTC': 30, u'AMC/BTC': 43, u'TIX/LTC': 107, u'RYC/LTC': 37, u'CNC/BTC': 8, u'MEM/LTC': 56, u'DGC/BTC': 26, u'ORB/BTC': 75, u'LKY/BTC': 34, u'XPM/BTC': 63, u'CAT/BTC': 136, u'MNC/BTC': 7, u'DVC/LTC': 52, u'LOT/BTC': 137, u'ARG/BTC': 48, u'JKC/LTC': 35, u'NRB/BTC': 54, u'DEM/BTC': 131, u'ELP/LTC': 93, u'GLD/LTC': 36, u'WDC/BTC': 14, u'IFC/LTC': 60, u'CGB/LTC': 123, u'RED/LTC': 87, u'HBN/BTC': 80, u'ELC/BTC': 12, u'XPM/LTC': 106, u'DGC/LTC': 96, u'DVC/XPM': 122, u'GDC/BTC': 82, u'COL/LTC': 109, u'PXC/LTC': 101, u'PYC/BTC': 92, u'EZC/LTC': 55, u'ANC/LTC': 121, u'NET/LTC': 108, u'BCX/BTC': 142, u'ASC/LTC': 111, u'FST/BTC': 44, u'KGC/BTC': 65, u'SBC/LTC': 128, u'MOON/BTC': 146, u'LK7/BTC': 116, u'FST/LTC': 124, u'ZET/LTC': 127, u'TEK/BTC': 114, u'SPT/BTC': 81, u'MEC/LTC': 100, u'YBC/BTC': 73, u'BTE/BTC': 49, u'CNC/LTC': 17, u'TIX/XPM': 103, u'SXC/LTC': 98, u'RPC/BTC': 143, u'QRK/LTC': 126, u'PPC/LTC': 125, u'DMD/BTC': 72, u'NMC/BTC': 29, u'GME/LTC': 84, u'COL/XPM': 110, u'TGC/BTC': 130, u'NBL/BTC': 32, u'NAN/BTC': 64, u'EAC/BTC': 139, u'IFC/XPM': 105, u'FFC/BTC': 138, u'ALF/BTC': 57, u'PXC/BTC': 31, u'DOGE/BTC': 132, u'FLO/LTC': 61, u'BQC/BTC': 10, u'CENT/XPM': 118, u'GLX/BTC': 78, u'BET/BTC': 129}
 
+
+def retry_request(func, *args, **kwargs):
+    sleep_retry = 1
+    times = 2
+    for _ in range(times):
+        try:
+            response = func(*args, **kwargs)
+            if int(response['success']) == 1:
+                return response
+            else:
+                print response
+                raise urllib2.HTTPError()
+        except urllib2.HTTPError as e:
+            print "%s; Retrying in %s seconds" % (e, sleep_retry)
+            time.sleep(sleep_retry)
+    print "Failed %s times; raising" % times
+    raise
 
 class CryptsyTrade(object):
     def __init__(self, data):
@@ -162,25 +180,32 @@ class ChaChing(object):
         self._all_markets_cache.reverse()
 
     def my_orders(self, market):
-        response = self.exchange.myOrders(MARKET_IDS[market])
-        if response['success'] == '1':
-            orders = [CryptsyOrder(
-                order, order['ordertype'], MARKET_IDS[market], created=order['created'], orderid=order['orderid'], exchange=self.exchange
+        response = retry_request(self.exchange.myOrders, MARKET_IDS[market])
+        orders = [CryptsyOrder(
+            order, order['ordertype'], MARKET_IDS[market], created=order['created'], orderid=order['orderid'], exchange=self.exchange
             ) for order in response['return']]
-            response = orders
-        else:
-            raise Exception("Failed retrieving orders!")
+        response = orders
         return response
 
+    def balances_available(self):
+        response = retry_request(self.exchange.getInfo)
+        balances = response['return']['balances_available']
+        return balances
+
+    def balance(self, coin):
+        balances = self.balances_available()
+        return balances[coin]
+
+    def openordercount(self):
+        response = retry_request(self.exchange.getInfo)
+        openordercount = response['return']['openordercount']
+        return openordercount
+
     def fetch_all_markets(self):
-        try:
-            market_data = self.exchange.getMarketDataV2()
-        except urllib2.HTTPError:
-            raise
-        else:
-            markets = CryptsyMarkets(market_data)
-            self._cache_all_markets(markets)
-            return markets
+        market_data = retry_request(self.exchange.getMarketDataV2)
+        markets = CryptsyMarkets(market_data)
+        self._cache_all_markets(markets)
+        return markets
 
     def get_all_markets(self):
         try:
@@ -190,14 +215,10 @@ class ChaChing(object):
         return markets
 
     def fetch_market(self, market):
-        try:
-            market_data = self.exchange.getSingleMarketData(MARKET_IDS[market])
-        except urllib2.HTTPError:
-            raise
-        else:
-            market = CryptsyMarket(market_data['return']['markets'].itervalues().next())
-            self._cache_market(market)
-            return market
+        market_data = retry_request(self.exchange.getSingleMarketData, MARKET_IDS[market])
+        market = CryptsyMarket(market_data['return']['markets'].itervalues().next())
+        self._cache_market(market)
+        return market
 
     def get_market(self, market):
         try:
@@ -225,4 +246,4 @@ class TradeBot2000(ChaChing):
     called once every 'trade_timer' seconds (default of 60)
     """
     def trade(self):
-        print self.get_market('DOGE/LTC')
+        pass
